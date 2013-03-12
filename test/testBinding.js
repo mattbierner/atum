4329b0d191e16226ff2742552bdff06246c45943
Matt Bierner <mattbierner@gmail.com>
2013-03-12 18:46:06 -0400

    Added forStatement semantics.

--- test/testBinding.js
+++ test/testBinding.js
@@ -24,6 +24,56 @@ function(value,
                 assert.equal(result.type, 'undefined');
                 assert.equal(result.value, undefined);
             }],
+            ["Simple Init Var",
+            function(){
+                var root = new program.Program([
+                    new declaration.VariableDeclaration([
+                         new declaration.VariableDeclarator(
+                             new value.Identifier(null, 'a'),
+                             new value.Literal(null, 10, "number"))]),
+                    new statement.ExpressionStatement(new value.Identifier(null, 'a'))]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 10);
+            }],
+            ["Multiple Variable Declaration",
+            function(){
+                var root = new program.Program([
+                    new declaration.VariableDeclaration([
+                         new declaration.VariableDeclarator(
+                             new value.Identifier(null, 'a'),
+                             new value.Literal(null, 1, "number")),
+                         new declaration.VariableDeclarator(
+                             new value.Identifier(null, 'b'),
+                             new value.Literal(null, 2, "number"))]),
+                    new statement.ExpressionStatement(new expression.BinaryExpression('+',
+                        new value.Identifier(null, 'a'),
+                        new value.Identifier(null, 'b')))]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 3);
+            }],
+            ["Multiple Variable Declaration",
+            function(){
+                var root = new program.Program([
+                    new declaration.VariableDeclaration([
+                         new declaration.VariableDeclarator(
+                             new value.Identifier(null, 'a'),
+                             new value.Literal(null, 1, "number"))]),
+                     new declaration.VariableDeclaration([
+                         new declaration.VariableDeclarator(
+                             new value.Identifier(null, 'b'),
+                             new value.Literal(null, 2, "number"))]),
+                    new statement.ExpressionStatement(new expression.BinaryExpression('+',
+                        new value.Identifier(null, 'a'),
+                        new value.Identifier(null, 'b')))]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 3);
+            }],
         ]
     };
 });
