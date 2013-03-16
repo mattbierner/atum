a4f63f6bf2316fe6ace8d85664b3306de5270312
Matt Bierner <mattbierner@gmail.com>
2013-03-16 14:46:03 -0400

    Added type conversion increment and decrement tests. Inc and dec always return a number instead of original type for postfix.

--- test/testIncDec.js
+++ test/testIncDec.js
@@ -13,131 +13,139 @@ function(value,
     
     var a = new value.Identifier(null, 'a');
     
+    
+    var set = [
+       new value.Literal(null, 0, "number"),
+       new value.Literal(null, 10, "number"),
+       new value.Literal(null, -10, "number"),
+       new value.Literal(null, 1.5, "number"),
+       new value.Literal(null, "1", "string")];
+    
     return {
         'module': "Test Increment and Decrement Operators",
         'tests': [
         // Prefix Increment 
             ["Prefix Increment Changes Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('++', a, true)),
-                    new statement.ExpressionStatement(a)]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, 1);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('++', a, true)),
+                        new statement.ExpressionStatement(a)]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value + 1);
+                });
             }],
             ["Prefix Increment Return Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('++', a, true))]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, 1);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('++', a, true))]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value + 1);
+                });
             }],
             
         // Prefix Decrement 
             ["Prefix Decrement Changes Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('--', a, true)),
-                    new statement.ExpressionStatement(a)]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, -1);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('--', a, true)),
+                        new statement.ExpressionStatement(a)]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value - 1);
+                });
             }],
             ["Prefix Decrement Return Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('--', a, true))]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, -1);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('--', a, true))]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value - 1);
+                });
             }],
             
          // Postfix Increment 
             ["Postfix Increment Changes Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('++', a, false)),
-                    new statement.ExpressionStatement(a)]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, 1);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('++', a, false)),
+                        new statement.ExpressionStatement(a)]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value + 1);
+                });
             }],
             ["Postfix Increment Return Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('++', a, false))]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, 0);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('++', a, false))]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value);
+                });
             }],
             
         // Postfix Decrement 
-            ["Postfix Increment Changes Value",
+            ["Postfix Decrement Changes Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('--', a, false)),
-                    new statement.ExpressionStatement(a)]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, -1);
+               set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('--', a, false)),
+                        new statement.ExpressionStatement(a)]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value - 1);
+                });
             }],
-            ["Postfix Increment Return Value",
+            ["Postfix Decrement Return Value",
             function(){
-                var root = new program.Program([
-                    new declaration.VariableDeclaration([
-                         new declaration.VariableDeclarator(
-                             a,
-                             new value.Literal(null, 0, "number"))]),
-                    new statement.ExpressionStatement(
-                        new expression.UpdateExpression('--', a, false))]);
-                
-                var result = interpret.interpret(root);
-                assert.equal(result.type, 'number');
-                assert.equal(result.value, 0);
+                set.forEach(function(x){
+                    var root = new program.Program([
+                        new declaration.VariableDeclaration([
+                             new declaration.VariableDeclarator(a, x)]),
+                        new statement.ExpressionStatement(
+                            new expression.UpdateExpression('--', a, false))]);
+                    
+                    var result = interpret.interpret(root);
+                    assert.equal(result.type, 'number');
+                    assert.equal(result.value, +x.value);
+                });
             }],
             
         ],
