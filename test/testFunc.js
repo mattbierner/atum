9c043c1251f403918d50172222d9477d294745fd
Matt Bierner <mattbierner@gmail.com>
2013-03-17 19:14:58 -0400

    Fixed function calls for closure support.

--- test/testFunc.js
+++ test/testFunc.js
@@ -77,8 +77,76 @@ function(value,
                 assert.equal(result.type, 'number');
                 assert.equal(result.value, 4);
             }],
+            ["Undefined Argument",
+            function(){
+                var root = new program.Program([
+                    new declaration.FunctionDeclaration(
+                        a,
+                        [b],
+                        new statement.BlockStatement([
+                            new statement.ReturnStatement(b)])),
+                    new expression.CallExpression(a, [])]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'undefined');
+                assert.equal(result.value, undefined);
+            }],
+            ["Argument Scope Leak Check",
+            function(){
+                // Make sure bound arguments are not accessible in calling scope.
+                var root = new program.Program([
+                    new declaration.FunctionDeclaration(
+                        a,
+                        [b],
+                        new statement.BlockStatement([])),
+                    new expression.CallExpression(a, [new value.Literal(null, 1, "number")]),
+                    new statement.ExpressionStatement(b)]);
+                
+                assert.throws(interpret.interpret.bind(undefined, root));
+            }],
             
-            
+            ["Argument Alias Scope Check",
+            function(){
+                // Make sure closest bound value for argument is used.
+                var root = new program.Program([
+                    new statement.ExpressionStatement(
+                        new expression.AssignmentExpression('=', b, new value.Literal(null, 100, "number"))),
+                    new declaration.FunctionDeclaration(
+                        a,
+                        [b],
+                        new statement.BlockStatement([
+                          new statement.ReturnStatement(b)])),
+                    new expression.CallExpression(a, [new value.Literal(null, 1, "number")])]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 1);
+            }],
+            ["Dynamic Resolution In Scope Check",
+            function(){
+                // Check that variables in function scope are resolved to current
+                // values not values when function declared.
+                var root = new program.Program([
+                    new declaration.VariableDeclaration([
+                         new declaration.VariableDeclarator(b)]),
+                    new declaration.FunctionDeclaration(
+                        a,
+                        [],
+                        new statement.BlockStatement([
+                          new statement.ReturnStatement(b)])),
+                    new statement.ExpressionStatement(
+                        new expression.AssignmentExpression('=', b, new value.Literal(null, 1, "number"))),
+                    new expression.BinaryExpression('+',
+                        // b resolves to 1
+                        new expression.CallExpression(a, []), 
+                        // b resolves to 3
+                        new expression.CallExpression(a, [
+                               new expression.AssignmentExpression('=', b, new value.Literal(null, 3, "number"))]))]);
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 4);
+            }],
             
         ]
     };
