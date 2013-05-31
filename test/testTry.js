c2b3e1a1c352e591843e2ba9e8270017f141695c
Matt Bierner <mattbierner@gmail.com>
2013-05-31 00:17:04 -0700

    Added basic tests for finally only try

--- test/testTry.js
+++ test/testTry.js
@@ -31,13 +31,53 @@ function($,
                         $.Block(
                             $.Expression($.PostIncrement(b)),
                             $.Throw($.Number(10))),
-                    $.Catch(a,
-                        $.Block(
-                            $.Expression($.Add(a, b))))));
+                        $.Catch(a,
+                            $.Block(
+                                $.Expression($.Add(a, b))))));
                 
                 var result = interpret.interpret(root);
                 assert.equal(result.type, 'number');
                 assert.equal(result.value, 11);
+            }],
+            ["Throw finally no catch",
+            function(){
+                var root = $.Program(
+                    $.Assign(b, $.Number(1)),
+                    $.Try(
+                        $.Block(
+                            $.Try(
+                                $.Block(
+                                    $.Expression($.PostIncrement(b)),
+                                    $.Throw($.Number(10)),
+                                    $.Expression($.PostIncrement(b))),
+                                null,
+                                $.Block(
+                                    $.Expression($.Assign(b, $.Mul(b, b)))))),
+                        $.Catch(a, $.Block())),
+                    $.Expression(b));
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 4);
+            }],
+            ["No throw finally no catch",
+            function(){
+                var root = $.Program(
+                    $.Assign(b, $.Number(1)),
+                    $.Try(
+                        $.Block(
+                            $.Try(
+                                $.Block(
+                                    $.Expression($.PostIncrement(b))),
+                                null,
+                                $.Block(
+                                    $.Expression($.Assign(b, $.Mul(b, b)))))),
+                        $.Catch(a, $.Block())),
+                    $.Expression(b));
+                
+                var result = interpret.interpret(root);
+                assert.equal(result.type, 'number');
+                assert.equal(result.value, 2);
             }],
            
         ]
