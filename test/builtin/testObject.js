26ee1aaaefadb1b9efd4c8976b0de3af72287cc2
Matt Bierner <mattbierner@gmail.com>
2013-08-04 23:07:39 -0700

    Added defineProperty enumerable test

--- test/builtin/testObject.js
+++ test/builtin/testObject.js
@@ -8,6 +8,7 @@ function($,
         c = $.Id('c'),
         Object = $.Id('Object'),
         defineProperty = $.Member(Object, $.Id('defineProperty')),
+        length = $.Id('length');
         keys = $.Member(Object, $.Id('keys'));
 
 
@@ -32,8 +33,35 @@ function($,
                     .test($.Expression($.Member(a, b)))
                         .type('number', 1)
                     
+                    .test($.Expression($.Member($.Call(keys, [a]), length)))
+                        .type('number', 0);
+            }],
+            ["Object.defineProperty set enumerable",
+            function(){
+                expect.run(
+                    $.Program(
+                        $.Expression($.Assign(a,
+                            $.Call(defineProperty, [
+                                $.Object(),
+                                $.String('b'),
+                                $.Object({
+                                    'kind': 'init',
+                                    'key': $.String('value'),
+                                    'value': $.Number(1)
+                                }, {
+                                    'kind': 'init',
+                                    'key': $.String('enumerable'),
+                                    'value': $.Boolean(true)
+                                })])))))
+                    
                     .test($.Expression($.Member(a, b)))
-                        .type('number', 1);
+                        .type('number', 1)
+                    
+                    .test($.Expression($.Member($.Call(keys, [a]), length)))
+                        .type('number', 1)
+                        
+                .test($.Expression($.ComputedMember($.Call(keys, [a]), $.Number(0))))
+                        .type('string', 'b');
             }],
         ]
     };
